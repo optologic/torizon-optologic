9a523ef8e0b7a2c6a6efc1a9ded97bbe786aa3da
Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>
2026-04-10 15:41:33 +0200

    torizon-optologic: update to Toradex BSP 7.5.0
    Update the Toradex BSP version and build number for all machines to
    7.5.0 and 30 respectively. Update the linux version accordingly.
    As the version and build number are now the same for all machines,
    remove the different version handling from the render.sh script and
    directly use the values in the YAML templates.
    
    Change-Id: Ib55df5c5af6208142aa418004a2cb87626ad8748
    Signed-off-by: Kévin L'hôpital <kevin.lhopital@savoirfairelinux.com>

--- template/render.sh
+++ template/render.sh
@@ -14,16 +14,6 @@ DISPLAYS=(
   10inch-SCX1001511GGC49
 )
 
-# Specific Toradex BSP version and build per machine
-declare -A TORADEX_BSP_VERSION_ARRAY
-TORADEX_BSP_VERSION_ARRAY["default"]='7.3.0'
-TORADEX_BSP_VERSION_ARRAY["verdin-imx95"]='7.4.0'
-
-# Specific Toradex BSP version and build per machine
-declare -A TORADEX_BSP_BUILD_ARRAY
-TORADEX_BSP_BUILD_ARRAY["default"]='18'
-TORADEX_BSP_BUILD_ARRAY["verdin-imx95"]='28'
-
 # Optional BASE_DEVICETREE per machine
 declare -A BASE_DEVICETREE_ARRAY
 BASE_DEVICETREE_ARRAY["colibri-imx8x"]='custom: "linux-toradex/arch/arm64/boot/dts/freescale/imx8qxp-colibri-iris-v2.dts"'
@@ -36,29 +26,11 @@ OUTDIR=${OUTDIR:-.}
 
 # Generate all combinations
 for m in "${MACHINES[@]}"; do
-
-  set +u
-
-  if [ -n "${TORADEX_BSP_VERSION_ARRAY[$m]}" ]; then
-    TORADEX_BSP_VERSION="${TORADEX_BSP_VERSION_ARRAY[$m]}"
-  else
-    TORADEX_BSP_VERSION="${TORADEX_BSP_VERSION_ARRAY["default"]}"
-  fi
-
-  if [ -n "${TORADEX_BSP_BUILD_ARRAY[$m]}" ]; then
-    TORADEX_BSP_BUILD="${TORADEX_BSP_BUILD_ARRAY[$m]}"
-  else
-    TORADEX_BSP_BUILD="${TORADEX_BSP_BUILD_ARRAY["default"]}"
-  fi
-
-  set -u
-
   for d in "${DISPLAYS[@]}"; do
     out="$OUTDIR/${m}_optologic_panel-cap-touch-${d}-lvds.yaml"
     BASE_DEVICETREE="${BASE_DEVICETREE_ARRAY["$m"]}"
     env MACHINE="$m" DISPLAY="$d" BASE_DEVICETREE="$BASE_DEVICETREE" \
-        TORADEX_BSP_VERSION="$TORADEX_BSP_VERSION" TORADEX_BSP_BUILD="$TORADEX_BSP_BUILD" \
-      envsubst '${MACHINE} ${DISPLAY} ${BASE_DEVICETREE} ${TORADEX_BSP_VERSION} ${TORADEX_BSP_BUILD}' < "$TPL" > "$out"
+        envsubst '${MACHINE} ${DISPLAY} ${BASE_DEVICETREE}' < "$TPL" > "$out"
     echo "Rendered $out"
   done
 done
